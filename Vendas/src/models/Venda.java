d9508f3fbc49486da5baa207589e7b4090dd863c
Diogo Deconto <dideconto@gmail.com>
2020-04-08 12:25:34 -0300

    Modelo de Venda com vários produtos

--- Vendas/src/models/Venda.java
+++ Vendas/src/models/Venda.java
@@ -1,36 +1,29 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Venda {
 
 	//Criando em relacionamento entre Venda e Cliente: composição ou é um agregação
 	//cliente, funcionário e produto -> Agregação
 	private Cliente cliente;
 	private Funcionario funcionario;
-	private Produto produto;
-	private int quantidade;
+	private ArrayList<ItemVenda> ItensVenda;	
 	
+	public ArrayList<ItemVenda> getItensVenda() {
+		return ItensVenda;
+	}
+
+	public void setItensVenda(ArrayList<ItemVenda> itensVenda) {
+		ItensVenda = itensVenda;
+	}
+
 	public Funcionario getFuncionario() {
 		return funcionario;
 	}
 
 	public void setFuncionario(Funcionario funcionario) {
 		this.funcionario = funcionario;
-	}
-
-	public Produto getProduto() {
-		return produto;
-	}
-
-	public void setProduto(Produto produto) {
-		this.produto = produto;
-	}
-
-	public int getQuantidade() {
-		return quantidade;
-	}
-
-	public void setQuantidade(int quantidade) {
-		this.quantidade = quantidade;
 	}
 
 	public Cliente getCliente() {
